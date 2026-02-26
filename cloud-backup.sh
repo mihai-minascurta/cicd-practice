8dda74ede8e90e2c71533a892138edf2df81fa9c
mihai <mihai@gmail.com>
2026-02-27 00:51:46 +0200

    Fixing bugs on cloud-backup.sh

--- cloud-backup.sh
+++ cloud-backup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo "Starting secure cloud backup..."
 
-if [ $AWS_ACCESS_KEY == "Production_Key_999" ]
-then
+if [ "$AWS_ACCESS_KEY" == "Production_Key_999" ]; then
   echo "Access Granted. Uploading backup..."
   echo "Secret used: $AWS_ACCESS_KEY"
 else
