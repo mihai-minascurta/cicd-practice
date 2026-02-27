7d8a0f38aa6bbed81b74e08f3d4fe04003edd383
mihai <mihai@gmail.com>
2026-02-27 15:16:56 +0200

    Adding a health script

---
+++ api-health.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Pinging production API..."
+
+if [ $API_KEY == "SuperSecret123" ]
+then
+  echo "200 OK: API is healthy!"
+  echo "Key used: $API_KEY"
+else
+  echo "403 Forbidden: Bad Key!"
+  exit 1
+fi
